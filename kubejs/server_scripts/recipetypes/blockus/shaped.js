2e8346af742d495ae184968bbee4f85b58b8c769
AK9 <anthonkrisat@hotmail.com>
2021-12-11 16:00:24 +0100

    updated unification to work with latest kjs & tr

--- kubejs/server_scripts/recipetypes/blockus/shaped.js
+++ kubejs/server_scripts/recipetypes/blockus/shaped.js
@@ -5,29 +5,29 @@
 onEvent('recipes', (event) => {
 
   // Wooden Bucket / Small Logs
-  event.shaped(item.of("blockus:oak_small_logs", 3), [
+  event.shaped("3x blockus:oak_small_logs", [
     ["minecraft:oak_log", "minecraft:oak_log", "minecraft:oak_log"],
   ]);
-  event.shaped(item.of("blockus:spruce_small_logs", 3), [
+  event.shaped("3x blockus:spruce_small_logs", [
     ["minecraft:spruce_log", "minecraft:spruce_log", "minecraft:spruce_log"],
   ]);
-  event.shaped(item.of("blockus:birch_small_logs", 3), [
+  event.shaped("3x blockus:birch_small_logs", [
     ["minecraft:birch_log", "minecraft:birch_log", "minecraft:birch_log"],
   ]);
-  event.shaped(item.of("blockus:jungle_small_logs", 3), [
+  event.shaped("3x blockus:jungle_small_logs", [
     ["minecraft:jungle_log", "minecraft:jungle_log", "minecraft:jungle_log"],
   ]);
-  event.shaped(item.of("blockus:acacia_small_logs", 3), [
+  event.shaped("3x blockus:acacia_small_logs", [
     ["minecraft:acacia_log", "minecraft:acacia_log", "minecraft:acacia_log"],
   ]);
-  event.shaped(item.of("blockus:dark_oak_small_logs", 3), [
+  event.shaped("3x blockus:dark_oak_small_logs", [
     [
       "minecraft:dark_oak_log",
       "minecraft:dark_oak_log",
       "minecraft:dark_oak_log",
     ],
   ]);
-  event.shaped(item.of("blockus:white_oak_small_logs", 3), [
+  event.shaped("3x blockus:white_oak_small_logs", [
     ["blockus:white_oak_log", "blockus:white_oak_log", "blockus:white_oak_log"],
   ]);
  
--- kubejs/server_scripts/recipetypes/minecraft/shaped.js
+++ kubejs/server_scripts/recipetypes/minecraft/shaped.js
@@ -8,7 +8,7 @@
 onEvent('recipes', (event) => {
 
   // Cobweb
-  event.shaped(item.of("minecraft:cobweb"), [
+  event.shaped("minecraft:cobweb", [
     ["minecraft:string", "minecraft:string", "minecraft:string"],
     ["minecraft:string", "minecraft:string", "minecraft:string"],
     ["minecraft:string", "minecraft:string", "minecraft:string"],
@@ -22,7 +22,7 @@ onEvent('recipes', (event) => {
   );
 
     // Sticks
-  event.shaped(item.of("minecraft:stick", 16), [
+  event.shaped("16x minecraft:stick", [
     ["#minecraft:logs"],
     ["#minecraft:logs"],
   ]);
@@ -30,7 +30,7 @@ onEvent('recipes', (event) => {
 
   // Hopper
   event.remove({ id: "charm:variant_chests/hopper" });
-    event.shaped(item.of("minecraft:hopper"), [
+    event.shaped("minecraft:hopper", [
       ["minecraft:iron_ingot", "#minecraft:logs",     "minecraft:iron_ingot"],
       ["minecraft:iron_ingot", "#minecraft:logs",     "minecraft:iron_ingot"],
       [null,                   "minecraft:iron_ingot", null],
