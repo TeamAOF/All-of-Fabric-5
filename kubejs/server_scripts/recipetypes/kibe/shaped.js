f024750b3c87b117de7af7198040af6114c0069f
AK9 <anthonkrisat@hotmail.com>
2021-12-19 15:37:00 +0100

    altered angel ring recipe, now made in runic altar

--- kubejs/server_scripts/recipetypes/kibe/shaped.js
+++ kubejs/server_scripts/recipetypes/kibe/shaped.js
@@ -7,11 +7,6 @@
 
 onEvent('recipes', (event) => {
 
-  // Angel Ring
-  event.shaped("kibe:angel_ring", [
-    ['kibe:glider_left_wing',                'minecraft:end_crystal',             'kibe:glider_right_wing'],
-    ['minecraft:elytra',                     'kibe:diamond_ring',                 'minecraft:elytra'],
-    ['conjuring:stabilized_conjuring_focus', 'techreborn:superconductor_upgrade', 'conjuring:stabilized_conjuring_focus'],
-  ]);
+
 
 });
