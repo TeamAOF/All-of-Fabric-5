3845fc321976d74931d9ed24ec34cb292cb4f10e
APACHƐ™ <anthonkrisat@hotmail.com>
2021-12-31 03:09:27 +0100

    MI salt conversion for BW rituals

---
+++ kubejs/server_scripts/recipetypes/bewitchment/shaped.js
@@ -0,0 +1,10 @@
+////////////////////////
+/// Made by Team AOF ///
+////////////////////////
+
+onEvent('recipes', (event) => {
+
+  // Salt Dust
+  event.shaped("2x bewitchment:salt", [["modern_industrialization:salt_dust"],["modern_industrialization:salt_dust"]]);
+ 
+});
