0a58c4f01bb951cdcbc06a2dfe0c3df600070616
APACHƐ™ <anthonkrisat@hotmail.com>
2022-01-11 09:27:26 +0100

    replace input for mi iron gear

--- kubejs/server_scripts/replace_inputs.js
+++ kubejs/server_scripts/replace_inputs.js
@@ -1,4 +1,5 @@
 onEvent('recipes', (event) => {
 
     event.replaceInput({}, '#c:wooden_chests', '#c:wooden_chests');
+    event.replaceInput({}, 'mtmechs:iron_gear_item', 'modern_industrialization:iron_gear');
 });
