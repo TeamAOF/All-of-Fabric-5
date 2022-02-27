a9a6c53f91b2fd63858e9b104ae4c1546afe690e
APACHƐ™ <anthonkrisat@hotmail.com>
2022-02-27 04:35:24 +0100

    fixed inputs of steel armor

--- kubejs/server_scripts/replace_inputs.js
+++ kubejs/server_scripts/replace_inputs.js
@@ -1,11 +1,11 @@
 onEvent('recipes', (event) => {
 
+    
+    event.replaceInput({}, '#c:steel_helmets', 'techreborn:steel_helmet');
+    event.replaceInput({}, '#c:steel_chestplates', 'techreborn:steel_chestplate');
+    event.replaceInput({}, '#c:steel_leggings', 'techreborn:steel_leggings');
+    event.replaceInput({}, '#c:steel_boots', 'techreborn:steel_boots');
     event.replaceInput({}, '#c:wooden_chests', '#c:wooden_chests');
-    event.replaceInput({}, '#c:iron_ingots', 'minecraft:iron_ingot');
     event.replaceInput({}, 'mtmechs:iron_gear_item', 'modern_industrialization:iron_gear');
-    event.replaceInput({}, 'indrev:steel_helmet', 'techreborn:steel_helmet');
-    event.replaceInput({}, 'indrev:steel_chestplate', 'techreborn:steel_chestplate');
-    event.replaceInput({}, 'indrev:steel_leggings', 'techreborn:steel_leggings');
-    event.replaceInput({}, 'indrev:steel_boots', 'techreborn:steel_boots');
     
 });
