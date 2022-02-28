22f8b006ce46b3f2da705b1d8d976186618d6125
APACHƐ™ <anthonkrisat@hotmail.com>
2022-02-28 03:09:46 +0100

    merged chromium into unification

--- kubejs/server_scripts/replace_inputs.js
+++ kubejs/server_scripts/replace_inputs.js
@@ -7,5 +7,12 @@ onEvent('recipes', (event) => {
     event.replaceInput({}, '#c:steel_boots', 'techreborn:steel_boots');
     event.replaceInput({}, '#c:wooden_chests', '#c:wooden_chests');
     event.replaceInput({}, 'mtmechs:iron_gear_item', 'modern_industrialization:iron_gear');
+
+    // Chromium
+    event.replaceInput({}, 'techreborn:chrome_ingot', 'modern_industrialization:chromium_ingot');
+    event.replaceInput({}, 'techreborn:chrome_nugget', 'modern_industrialization:chromium_nugget');
+    event.replaceInput({}, 'techreborn:chrome_dust', 'modern_industrialization:chromium_dust');
+    event.replaceInput({}, 'techreborn:chrome_plate', 'modern_industrialization:chromium_plate');
+    event.replaceInput({}, 'techreborn:chrome_storage_block', 'modern_industrialization:chromium_block');
     
 });
--- kubejs/server_scripts/replace_outputs.js
+++ kubejs/server_scripts/replace_outputs.js
@@ -1,4 +1,12 @@
 onEvent('recipes', (event) => {
 
     event.replaceOutput({}, 'crookedcrooks:ir_bronze_crook', 'crookedcrooks:tr_bronze_crook');
+
+    // Chromium
+    event.replaceOutput({}, 'techreborn:chrome_ingot', 'modern_industrialization:chromium_ingot');
+    event.replaceOutput({}, 'techreborn:chrome_nugget', 'modern_industrialization:chromium_nugget');
+    event.replaceOutput({}, 'techreborn:chrome_dust', 'modern_industrialization:chromium_dust');
+    event.replaceOutput({}, 'techreborn:chrome_plate', 'modern_industrialization:chromium_plate');
+    event.replaceOutput({}, 'techreborn:chrome_storage_block', 'modern_industrialization:chromium_block');
+
 });
