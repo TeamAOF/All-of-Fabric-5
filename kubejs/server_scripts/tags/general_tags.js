3082531f34867c86ccf2e213b5477bfe5520f566
APACHƐ™ <anthonkrisat@hotmail.com>
2022-03-09 20:30:55 +0100

    milk unification

--- kubejs/server_scripts/tags/general_tags.js
+++ kubejs/server_scripts/tags/general_tags.js
@@ -6,7 +6,13 @@
 onEvent('tags.items', event => {
 
   event.removeAllTagsFrom('betterend:thallasium_ingot')
+  
   event.add('minecraft:coals', 'modern_industrialization:lignite_coal')
+
   event.add('c:workbench', 'minecraft:crafting_table')
+
+  event.add('fabric:milk', 'croptopia:milk_bottle')
+  event.add('fabric:milk', 'croptopia:soy_milk')
+  event.add('c:milks', 'farmersdelight:milk_bottle')
 
 });
